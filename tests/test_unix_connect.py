63827ebb9473b74a4702475128f54754d354858b
dmachard <5562930+dmachard@users.noreply.github.com>
2021-10-06 08:23:28 +0200

    tests for unix socket

--- tests/test_unix_connect.py
+++ tests/test_unix_connect.py
@@ -4,7 +4,7 @@ import unittest
 class TestUnixConnect(unittest.TestCase):
     def test_status(self):
         """connect and get status"""             
-        rc = RemoteControl(unix_sock="/tmp/unbound-console.sock")
+        rc = RemoteControl(unix_sock="/tmp/unbound/unbound-console.sock")
         o = rc.send_command(cmd="status")
         print(o)
         self.assertRegex(o, ".*is running.*")
