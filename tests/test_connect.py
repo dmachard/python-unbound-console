eb6006d9e3bc67babc580e9f0fe4a4c29e0d076e
Leo Spratt <enchant97@users.noreply.github.com>
2022-06-29 10:35:08 +0100

    add async unit tests

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -1,4 +1,4 @@
-from unbound_console import RemoteControl
+from unbound_console import RemoteControl, RemoteControlAsync
 import unittest
 
 class TestTcpConnect(unittest.TestCase):
@@ -6,5 +6,14 @@ class TestTcpConnect(unittest.TestCase):
         """connect and get status"""             
         rc = RemoteControl(host="127.0.0.1", port=8953)
         o = rc.send_command(cmd="status")
+        print(o)
+        self.assertRegex(o, ".*is running.*")
+
+
+class TestAsyncTcpConnect(unittest.IsolatedAsyncioTestCase):
+    async def test_status(self):
+        """connect and get status"""
+        rc = RemoteControlAsync(host="127.0.0.1", port=8953)
+        o = await rc.send_command(cmd="status")
         print(o)
         self.assertRegex(o, ".*is running.*")
--- tests/test_tls_connect.py
+++ tests/test_tls_connect.py
@@ -1,4 +1,4 @@
-from unbound_console import RemoteControl
+from unbound_console import RemoteControl, RemoteControlAsync
 import unittest
 
 class TestTlsConnect(unittest.TestCase):
@@ -9,5 +9,17 @@ class TestTlsConnect(unittest.TestCase):
                            client_cert= "/tmp/unbound_control.pem",
                            client_key= "/tmp/unbound_control.key")
         o = rc.send_command(cmd="status")
+        print(o)
+        self.assertRegex(o, ".*is running.*")
+
+
+class TestAsyncTlsConnect(unittest.IsolatedAsyncioTestCase):
+    async def test_status(self):
+        """connect and get status"""
+        rc = RemoteControlAsync(host="127.0.0.1", port=8953,
+                           server_cert = "/tmp/unbound_server.pem",
+                           client_cert= "/tmp/unbound_control.pem",
+                           client_key= "/tmp/unbound_control.key")
+        o = await rc.send_command(cmd="status")
         print(o)
         self.assertRegex(o, ".*is running.*")
